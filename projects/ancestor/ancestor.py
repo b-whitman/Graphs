43ec9fca24f5a9835c03a25d66b3cf33690ef01a
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-11-13 23:45:25 -0500

    ancestor pseudocode

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,15 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # node = starting_node
+    # Find the parents of node
+    # If node has no parents, return -1
+    # while len(parents) > 0,
+    ## new_parents = parents of parents
+    ## if len(new_parents) == 1 and new_parents has no parents,
+    ### return new_parents[0]
+    ## elif len(new_parents) > 1
+    ### parents = new_parents
+    ## elif len(new_parents) == 0
+    ### return min value in new_parents
+    # Once while loop exits,
+    # return min value in new_parents
